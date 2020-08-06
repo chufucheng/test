fe18121887544ee90895e4e44d192dd135d66ebf
chufucheng <chufucheng@100tal.com>
2020-08-06 12:34:36 +0800

    add:ceshi

--- excel.php
+++ excel.php
@@ -1,4 +1,10 @@
 <?php
+    //载入PHPExcel类文件
+//    require_once './PHPExcel-1.8/Classes/PHPExcel.php';
+//    //实例化PHPExcel类对象
+//    $phpExcel = new PHPExcel();
+    //创建PHPExcel写入类
+//    $writeExcel = new
     $mysqli = new mysqli();
     $mysqli->connect('localhost','root','','information_schema');
 
@@ -12,14 +18,14 @@
         }
     }
     foreach($tableList as $tableName) {
-        $sql = "SELECT COLUMN_NAME 字段名称, 
-       COLUMN_COMMENT 中文说明, 
-       (CASE WHEN column_key = 'PRI' THEN 'PK' ELSE '' END) AS 键别, 
-       (CASE WHEN is_nullable = 'NO' THEN 'NO' ELSE 'YES' END) AS 是否空, 
-       COLUMN_TYPE 数据类型类型, CHARACTER_MAXIMUM_LENGTH AS 长度, 
-       NUMERIC_PRECISION 数字精度, 
-       NUMERIC_SCALE 小数位数 
-FROM INFORMATION_SCHEMA. COLUMNS 
+        $sql = "SELECT COLUMN_NAME 字段名称,
+       COLUMN_COMMENT 中文说明,
+       (CASE WHEN column_key = 'PRI' THEN 'PK' ELSE '' END) AS 键别,
+       (CASE WHEN is_nullable = 'NO' THEN 'NO' ELSE 'YES' END) AS 是否空,
+       COLUMN_TYPE 数据类型类型, CHARACTER_MAXIMUM_LENGTH AS 长度,
+       NUMERIC_PRECISION 数字精度,
+       NUMERIC_SCALE 小数位数
+FROM INFORMATION_SCHEMA. COLUMNS
 WHERE table_schema = 'extend' AND table_name " . ' = ' . "'" . $tableName . "'";
 //        print_r($sql);die;
         $rs = $mysqli->query($sql);
